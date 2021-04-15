7e04736eef0c7d8449356179278b35d5215ce164
hamilton <hamilton.ulmer@gmail.com>
2021-04-15 14:12:23 -0700

    adds back data inspector and storage

---
+++ src/app/stories/study.stories.js
@@ -0,0 +1,9 @@
+import DisplayStory from './Display.svelte';
+
+export default {
+    title: "Rally Study 01 Inspector",
+};
+
+export const Display = () => ({
+    Component: DisplayStory,
+  });
--- src/study.js
+++ src/study.js
@@ -1,7 +1,8 @@
-import { Rally, runStates } from "@mozilla/rally";
-import { onPageData, stopMeasurement } from "./attention-reporter";
+// import { Rally, runStates } from "@mozilla/rally";
+import browser from "webextension-polyfill";
+import { onPageData } from "./attention-reporter";
   
-function collectEventDataAndSubmit(rally, devMode) {
+function collectEventDataAndSubmit(devMode) {
   // note: onPageData calls startMeasurement.
   onPageData.addListener(async (data) => {
     if (devMode) {
@@ -11,42 +12,54 @@ function collectEventDataAndSubmit(rally, devMode) {
     // we send the payload using one schema, "RS01.event".
     // Once https://github.com/mozilla-rally/web-science/issues/33 is resolved,
     // we will change the collection schema (but keep this pipeline schema the same).
-    rally.sendPing("RS01.event", data);
+    //rally.sendPing("RS01.event", data);
   }, {
       matchPatterns: ["<all_urls>"],
       privateWindows: false
   });
 }
 
-export default async function runStudy(devMode) {
-    const rally = new Rally();
-    try {
-      await rally.initialize(
-        "zero-one",
-        {
-          "crv": "P-256",
-          "kid": "zero-one",
-          "kty": "EC",
-          "x": "edhPpqhgK9dD7NaqhQ7Ckw9sU6b39X7XB8HnA366Rjs",
-          "y": "GzsfM19n-iH-DVR0iKEoA8BE2CFF46wR__siJ3SdiNs"
-        },          
-      devMode,
-      (newState) => {
-          if (newState === runStates.RUNNING) {
-          // if the study is running but wasn't previously, let's re-initiate the onPageData listener.
-          console.debug("~~~ RS01 running ~~~");
-          collectEventDataAndSubmit(rally, devMode);
-          } else {
-          console.debug("~~~ RS01 not running ~~~");
-          // stop the measurement here.
-          stopMeasurement();
-          }
-      })
-    } catch (err) {
-      throw new Error(err);
-    }
-    // if initialization worked, commence data collection.
-    console.debug("~~~ RS01 running ~~~");
-    collectEventDataAndSubmit(rally, devMode);
-    return rally;
+function openPage() {
+  browser.runtime.openOptionsPage().catch(e => {
+    console.error(`Study Add-On - Unable to open the control panel`, e);
+  });
 }
+
+export default async function runStory(devMode) {
+  collectEventDataAndSubmit(devMode);
+  
+  browser.browserAction.onClicked.addListener(openPage);
+}
+
+// export default async function runStudy(devMode) {
+//     const rally = new Rally();
+//     try {
+//       await rally.initialize(
+//         "zero-one",
+//         {
+//           "crv": "P-256",
+//           "kid": "zero-one",
+//           "kty": "EC",
+//           "x": "edhPpqhgK9dD7NaqhQ7Ckw9sU6b39X7XB8HnA366Rjs",
+//           "y": "GzsfM19n-iH-DVR0iKEoA8BE2CFF46wR__siJ3SdiNs"
+//         },          
+//       devMode,
+//       (newState) => {
+//           if (newState === runStates.RUNNING) {
+//           // if the study is running but wasn't previously, let's re-initiate the onPageData listener.
+//           console.debug("~~~ RS01 running ~~~");
+//           collectEventDataAndSubmit(rally, devMode);
+//           } else {
+//           console.debug("~~~ RS01 not running ~~~");
+//           // stop the measurement here.
+//           stopMeasurement();
+//           }
+//       })
+//     } catch (err) {
+//       throw new Error(err);
+//     }
+//     // if initialization worked, commence data collection.
+//     console.debug("~~~ RS01 running ~~~");
+//     collectEventDataAndSubmit(rally, devMode);
+//     return rally;
+// }
